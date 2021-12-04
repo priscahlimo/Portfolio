f9d121c00d4e3daa634c40e2820e24f3061b9380
priscahlimo <priscahlimoh@gmail.com>
2021-12-04 14:45:03 +0300

    add javascript file

---
+++ contact.js
@@ -0,0 +1,7 @@
+function contact(event){
+let search=document.querySelector("#exampleFormControlTextarea1")
+alert("Thank you for the feedback")
+}
+
+let form= document.querySelector("#form");
+form.addEventListener("submit", contact)
